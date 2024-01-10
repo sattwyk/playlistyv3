e34e1be37e33a4ef1f35b9c953b3185f91a1c71f
Sattwik <hellosattwik@gmail.com>
2024-01-11 05:20:26 +0530

    feat: Add sign in and dashboard buttons

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,23 @@
+import { Button } from '@/components/ui/button';
+import { SignInButton, SignedIn, SignedOut } from '@clerk/nextjs';
+import Link from 'next/link';
+
 export default function Home() {
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
       <h1 className='text-6xl'>Playlisty</h1>
 
       <p>Manage your youtube playlists without getting distracted</p>
+      <SignedIn>
+        <Link passHref href='/dashboard'>
+          <Button variant='secondary'>Dashboard</Button>
+        </Link>
+      </SignedIn>
+      <SignedOut>
+        <SignInButton mode='modal'>
+          <Button variant='default'>Sign in with Google</Button>
+        </SignInButton>
+      </SignedOut>
     </main>
   );
 }
