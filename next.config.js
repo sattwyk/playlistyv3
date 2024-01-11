c91d113c8d25c7ab00437b4e95c955dc39462e66
Sattwik <hellosattwik@gmail.com>
2024-01-12 04:18:24 +0530

    feat: Add remotePatterns to images in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        hostname: 'i.ytimg.com',
+      },
+      {
+        hostname: 'images.unsplash.com',
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
