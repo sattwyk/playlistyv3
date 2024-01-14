d57c672a39d230c560228fa7d11e27405c89a9e3
Sattwik <hellosattwik@gmail.com>
2024-01-14 14:02:29 +0530

    feat(landing): add landing page components

---
+++ app/(landing)/layout.tsx
@@ -0,0 +1,16 @@
+import { Footer } from './footer';
+import { Header } from './header';
+
+export default function LandingLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className='relative flex min-h-screen flex-col'>
+      <Header />
+      <main className='flex-1'>{children}</main>
+      <Footer />
+    </div>
+  );
+}
--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,23 +1,44 @@
-import { Button } from '@/components/ui/button';
-import { SignInButton, SignedIn, SignedOut } from '@clerk/nextjs';
 import Link from 'next/link';
+import { SignedIn, SignedOut } from '@clerk/nextjs';
 
-export default function Home() {
+import { siteConfig } from '@/config/site';
+import { buttonVariants } from '@/components/ui/button';
+
+export default function IndexPage() {
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      <h1 className='text-6xl'>Playlisty</h1>
-
-      <p>Manage your youtube playlists without getting distracted</p>
-      <SignedIn>
-        <Link passHref href='/dashboard'>
-          <Button variant='secondary'>Dashboard</Button>
+    <section className='container grid items-center gap-6 pb-8 pt-6 md:py-10'>
+      <div className='flex max-w-[980px] flex-col items-start gap-2'>
+        <h1 className='text-3xl font-extrabold leading-tight tracking-tighter sm:text-3xl md:text-5xl lg:text-6xl'>
+          Finally, a way to manage your YouTube playlists{' '}
+          <br className='hidden sm:inline' />
+          without getting distracted.
+        </h1>
+        <p className='max-w-[700px] text-lg text-muted-foreground sm:text-xl'>
+          Playlisty is the perfect tool for students, professionals, and anyone
+          who wants to stay on track and get things done. It&apos;s free and
+          open source, so you can use it to your heart&apos;s content.
+        </p>
+      </div>
+      <div className='flex gap-4'>
+        <SignedIn>
+          <Link href='/browse' className={buttonVariants({ size: 'lg' })}>
+            Browse
+          </Link>
+        </SignedIn>
+        <SignedOut>
+          <Link href='/sign-in' className={buttonVariants({ size: 'lg' })}>
+            Get Started
+          </Link>
+        </SignedOut>
+        <Link
+          target='_blank'
+          rel='noreferrer'
+          href={siteConfig.links.github}
+          className={buttonVariants({ variant: 'outline', size: 'lg' })}
+        >
+          GitHub
         </Link>
-      </SignedIn>
-      <SignedOut>
-        <SignInButton mode='modal'>
-          <Button variant='default'>Sign in with Google</Button>
-        </SignInButton>
-      </SignedOut>
-    </main>
+      </div>
+    </section>
   );
 }
