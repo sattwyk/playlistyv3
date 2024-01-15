9460d3d0f0808817a0be6d77ae8f0b6f4dd1d878
Sattwik <hellosattwik@gmail.com>
2024-01-15 19:40:27 +0530

    style: Remove commented out code and adjust grid layout in layout.tsx

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -19,12 +19,11 @@ export default async function AppLayout({ children }: LayoutProps) {
     <ProgressBar>
       <main className='min-h-screen bg-background'>
         <div className='md:block'>
-          {/* <Menu /> */}
           <div className='border-t'>
             <div className='bg-background'>
-              <div className='grid lg:grid-cols-5'>
-                <Sidebar className='lg:block' />
-                <div className='col-span-3 lg:col-span-4 lg:border-l'>
+              <div className='grid lg:grid-cols-7'>
+                <Sidebar className='lg:block lg:col-span-1' />
+                <div className='col-span-3 lg:col-span-6 lg:border-l'>
                   <div className='h-full px-4 py-6 lg:px-8'>{children}</div>
                 </div>
               </div>
