09ec304d7f5c35ed4ca379f85d0816df13d901a7
Sattwik <hellosattwik@gmail.com>
2024-01-16 02:28:51 +0530

    feat: add function to generate base64 blur image URL

--- utils/generate-blur-placeholder.ts
+++ utils/generate-blur-placeholder.ts
@@ -1,24 +1,23 @@
-'use server'
+'use server';
 
-import imagemin from "imagemin";
-import imageminJpegtran from "imagemin-jpegtran";
+import sharp from 'sharp';
+import { unstable_cache } from 'next/cache';
 
-const cache = new Map<string, string>();
-
-export async function getBase64ImageUrl(
+export const getBase64BlurImageUrl = unstable_cache(async (
     imageUrl: string,
-): Promise<string> {
-    let url = cache.get(imageUrl);
-    if (url) {
-        return url;
-    }
+): Promise<string> => {
+
     const response = await fetch(imageUrl);
     const buffer = await response.arrayBuffer();
-    const minified = await imagemin.buffer(Buffer.from(buffer), {
-        plugins: [imageminJpegtran()],
-    });
 
-    url = `data:image/jpeg;base64,${Buffer.from(minified).toString("base64")}`;
-    cache.set(imageUrl, url);
-    return url;
-}
+    // Create a blurred placeholder image using sharp
+    const blurred = await sharp(Buffer.from(buffer))
+        .resize(30) // Resize to a small image for the blur effect
+        .blur() // Apply the blur effect
+        .toBuffer();
+
+    const base64BlurUrl = `data:image/jpeg;base64,${blurred.toString('base64')}`;
+    return base64BlurUrl;
+}, ["blur-placeholder"], {
+    tags: ["blur-placeholder"]
+})
