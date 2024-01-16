ec05eea2550fa6ffce1cfe9e950cdfda1ab65702
Sattwik <hellosattwik@gmail.com>
2024-01-16 11:41:16 +0530

    fix(utils): handle error when fetching image

--- utils/generate-blur-placeholder.ts
+++ utils/generate-blur-placeholder.ts
@@ -3,21 +3,28 @@
 import sharp from 'sharp';
 import { unstable_cache } from 'next/cache';
 
+// TODO: Add default base64 url
+const defaultBase64BlurUrl = 'data:image/jpeg;base64,...'
+
 export const getBase64BlurImageUrl = unstable_cache(async (
     imageUrl: string,
 ): Promise<string> => {
+    try {
+        const response = await fetch(imageUrl);
+        const buffer = await response.arrayBuffer();
 
-    const response = await fetch(imageUrl);
-    const buffer = await response.arrayBuffer();
+        // Create a blurred placeholder image using sharp
+        const blurred = await sharp(Buffer.from(buffer))
+            .resize(30) // Resize to a small image for the blur effect
+            .blur() // Apply the blur effect
+            .toBuffer();
 
-    // Create a blurred placeholder image using sharp
-    const blurred = await sharp(Buffer.from(buffer))
-        .resize(30) // Resize to a small image for the blur effect
-        .blur() // Apply the blur effect
-        .toBuffer();
-
-    const base64BlurUrl = `data:image/jpeg;base64,${blurred.toString('base64')}`;
-    return base64BlurUrl;
+        const base64BlurUrl = `data:image/jpeg;base64,${blurred.toString('base64')}`;
+        return base64BlurUrl;
+    } catch (error) {
+        console.error('')
+        return defaultBase64BlurUrl
+    }
 }, ["blur-placeholder"], {
     tags: ["blur-placeholder"]
 })
