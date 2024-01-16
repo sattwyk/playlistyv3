e218db65d205e2e32016622aaee8cbe4b009aa07
Sattwik <hellosattwik@gmail.com>
2024-01-16 11:42:16 +0530

    feat(youtube): add caching for playlist videos

--- lib/youtube/videos.ts
+++ lib/youtube/videos.ts
@@ -4,12 +4,12 @@ import { google, youtube_v3 } from 'googleapis';
 import { z } from 'zod';
 import { getYouTubeThumbnail } from './thubmanail'
 import { getErrorMessage } from '@/utils';
-
+import { unstable_cache } from "next/cache"
 
 const VideoSchema = z.object({
     title: z.string(),
     description: z.string(),
-    videoId: z.string(),
+    youtubeVideoId: z.string(),
     channelId: z.string(),
     channelTitle: z.string(),
     publishedAt: z.string(),
@@ -18,21 +18,22 @@ const VideoSchema = z.object({
         url: z.string().url(),
         height: z.number(),
         width: z.number(),
+        blurDataURL: z.string()
     })
 });
 
 export type VideoInfo = z.infer<typeof VideoSchema>;
 
-function getVideosInfo(videos: youtube_v3.Schema$PlaylistItem[]): VideoInfo[] {
+async function getVideosInfo(videos: youtube_v3.Schema$PlaylistItem[]): Promise<VideoInfo[]> {
     console.log(`Extracting video information from ${videos.length} items`);
     const result: VideoInfo[] = [];
 
     for (const video of videos) {
-        const thumbnail = getYouTubeThumbnail(video.snippet?.thumbnails);
+        const thumbnail = await getYouTubeThumbnail(video.snippet?.thumbnails);
         const videoInfo = VideoSchema.safeParse({
             title: video.snippet?.title,
             description: video.snippet?.description,
-            videoId: video.snippet?.resourceId?.videoId,
+            youtubeVideoId: video.snippet?.resourceId?.videoId,
             channelId: video.snippet?.channelId,
             channelTitle: video.snippet?.channelTitle,
             publishedAt: video.snippet?.publishedAt,
@@ -41,7 +42,7 @@ function getVideosInfo(videos: youtube_v3.Schema$PlaylistItem[]): VideoInfo[] {
         });
 
         if (videoInfo.success) {
-            console.log(`Video information parsed successfully for video ID: ${videoInfo.data.videoId}`);
+            console.log(`Video information parsed successfully for video ID: ${videoInfo.data.youtubeVideoId}`);
             result.push(videoInfo.data);
         } else {
             console.error(`Failed to parse video information for a video: ${videoInfo.error}`);
@@ -54,7 +55,7 @@ function getVideosInfo(videos: youtube_v3.Schema$PlaylistItem[]): VideoInfo[] {
 
 
 
-export async function getPlaylistVideos(playlistId: string) {
+export const getPlaylistVideos = unstable_cache(async (playlistId: string) => {
     console.log(`Attempting to fetch videos for playlist ID: ${playlistId}`);
     try {
         const youtube = google.youtube("v3");
@@ -72,10 +73,12 @@ export async function getPlaylistVideos(playlistId: string) {
             return { error: "No Videos found", videos: null };
         } else {
             console.log(`Processing video information for playlist ID: ${playlistId}`);
-            return { videos: getVideosInfo(videos), error: null };
+            return { videos: await getVideosInfo(videos), error: null };
         }
     } catch (error) {
         console.error(`Error fetching videos for playlist ID: ${playlistId}: ${getErrorMessage(error)}`);
         return { error: getErrorMessage(error), videos: null };
     }
-}
+}, ['yt-playlist-videos'], {
+    tags: ['yt-playlist-videos']
+})
