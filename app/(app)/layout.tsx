6dec7265dee229848d8764cd2c66cf802424cae7
Sattwik <hellosattwik@gmail.com>
2024-01-16 22:26:25 +0530

    feat(layout): Add async fetching of user playlists and tags

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -4,6 +4,7 @@ import { Sidebar } from '@/components/sidebar';
 
 import React from 'react';
 import { ProgressBar } from '@/components/progress-bar';
+import { getUserPlaylists, getUserTags } from '@/actions';
 // import { revalidateTag } from 'next/cache';
 
 export const metadata: Metadata = {
@@ -16,6 +17,15 @@ interface LayoutProps {
 }
 
 export default async function AppLayout({ children }: LayoutProps) {
+  const [playlistsResult, userTagsResult] = await Promise.allSettled([
+    getUserPlaylists(),
+    getUserTags(),
+  ]);
+
+  const playlists =
+    playlistsResult.status === 'fulfilled' ? playlistsResult.value : null;
+  const userTags =
+    userTagsResult.status === 'fulfilled' ? userTagsResult.value : null;
   // revalidateTag('yt-user-playlists');
   return (
     <ProgressBar>
@@ -24,7 +34,11 @@ export default async function AppLayout({ children }: LayoutProps) {
           <div className='border-t'>
             <div className='bg-background'>
               <div className='grid lg:grid-cols-7'>
-                <Sidebar className='lg:block lg:col-span-1' />
+                <Sidebar
+                  playlists={playlists?.userPlaylists ?? []}
+                  userTags={userTags?.userTags ?? []}
+                  className='lg:block lg:col-span-1'
+                />
                 <div className='col-span-3 lg:col-span-6 lg:border-l'>
                   <div className='h-full px-4 py-6 lg:px-8'>{children}</div>
                 </div>
