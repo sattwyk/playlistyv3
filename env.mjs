fc7822151c2600189379cde05ce720e96727bee9
Sattwik <hellosattwik@gmail.com>
2024-01-16 16:59:15 +0530

    refactor: simplify skipValidation assignment

--- env.mjs
+++ env.mjs
@@ -41,5 +41,5 @@ export const env = createEnv({
   //   NEXT_PUBLIC_CLERK_AFTER_SIGN_UP_URL: process.env.NEXT_PUBLIC_CLERK_AFTER_SIGN_UP_URL,
   //   NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
   // }
-  skipValidation: process.env.CI ? true : false,
+  skipValidation: true,
 });
