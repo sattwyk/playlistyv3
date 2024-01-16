06c50c8812793e159a608cb1b7735bed5b73633e
Sattwik <hellosattwik@gmail.com>
2024-01-16 14:05:59 +0530

    feat: Add authToken to database client configuration

--- db/index.ts
+++ db/index.ts
@@ -1,8 +1,10 @@
 import { createClient } from "@libsql/client";
 import { drizzle } from "drizzle-orm/libsql";
+import { env } from "@/env.mjs"
 
 const client = createClient({
-    url: process.env.DATABASE_URL!
+    url: env.DATABASE_URL,
+    authToken: env.AUTH_TOKEN
 });
 
 export const db = drizzle(client);
