682e19770cb769065dbd18614011148a69150af9
Sattwik <hellosattwik@gmail.com>
2024-01-18 19:25:53 +0530

    fix(page): update SignIn and SignUp components after URLs

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -3,7 +3,7 @@ import { SignUp } from '@clerk/nextjs';
 export default function Page() {
   return (
     <div className='flex justify-center py-24'>
-      <SignUp />
+      <SignUp afterSignInUrl='/browse' afterSignUpUrl='/browse' />
     </div>
   );
 }
