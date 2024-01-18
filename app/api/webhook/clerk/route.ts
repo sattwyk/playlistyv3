6cfee83f236845430416db757da0bfb71e4acd22
Sattwik <hellosattwik@gmail.com>
2024-01-18 19:13:50 +0530

    feat: Add revalidation on user actions

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -6,6 +6,7 @@ import { db } from "@/db";
 import { users } from "@/db/schema";
 import { eq } from "drizzle-orm";
 import { env } from "@/env.mjs"
+import { revalidatePath } from "next/cache";
 
 
 const webhookSecret = env.CLERK_WEBHOOK_SECRET;
@@ -34,13 +35,14 @@ export async function POST(req: Request) {
                 const { id } = payload.data
                 await db.insert(users).values({ clerkId: id, updatedAt: new Date(Date.now()) })
                 console.log("Successfully created the user");
+                revalidatePath('/')
                 return NextResponse.json({ message: "Successfully created the user" })
-
             }
             case "user.updated": {
                 //TODO: Update user in the database
                 const { id } = payload.data
                 await db.update(users).set({ updatedAt: new Date(Date.now()) }).where(eq(users.clerkId, id))
+                revalidatePath('/')
                 return NextResponse.json({ message: "Successfully updated the user" })
 
             }
@@ -48,6 +50,7 @@ export async function POST(req: Request) {
                 const { id } = payload.data
                 await db.delete(users).where(eq(users.clerkId, id!));
                 console.log("Successfully deleted the user");
+                revalidatePath('/')
                 return NextResponse.json({ message: "Successfully deleted the user" })
             }
         }
